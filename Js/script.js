b57d557d2c46422772d839b4903642569604ee9a
danielsind <danielsindet@gmail.com>
2019-12-01 13:05:54 +0300

    rename var n to m. o to d

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,7 @@
 function mt(){
 alert("Thank you for your sumission");
-var n = parseInt(document.getElementsByTagName("select")[1].value);
-alert("Your month is " + n);
-var o = parseInt(document.getElementsByTagName("select")[0].value);
-alert("yours date is " + o);
+var m = parseInt(document.getElementsByTagName("select")[1].value);
+alert("Your month is " + m);
+var d = parseInt(document.getElementsByTagName("select")[0].value);
+alert("yours date is " + d);
 }
