5474bd3b899940d7037a7720aa66c3264ffa7356
danielsind <danielsindet@gmail.com>
2019-12-01 13:48:58 +0300

    edit the readme.md,create a gender variable

--- Js/script.js
+++ Js/script.js
@@ -4,4 +4,9 @@ var m = parseInt(document.getElementsByTagName("select")[1].value);
 alert("Your month is " + m);
 var d = parseInt(document.getElementsByTagName("select")[0].value);
 alert("yours date is " + d);
+var y = parseInt(document.getElementsByTagName("input")[0].value);
+alert("your year is " + y);
+var g = document.getElementsByTagName("select")[2].value;
+alert("yours gender is " + g);
+
 }
