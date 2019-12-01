ea51d1737a129574ac93edcd0a0a9b781b369598
danielsind <danielsindet@gmail.com>
2019-12-01 13:02:37 +0300

    declare date variable as integer

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,7 @@
 function mt(){
 alert("Thank you for your sumission");
-var n = document.getElementsByTagName("select")[1].value;
-alert("Your" + n);
+var n = parseInt(document.getElementsByTagName("select")[1].value);
+alert("Your month is " + n);
+var o = parseInt(document.getElementsByTagName("select")[0].value);
+alert("yours date is " + o);
 }
