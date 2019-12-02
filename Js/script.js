120df59c8cce0711b54334d7ecbe5ca507aec25e
danielsind <danielsindet@gmail.com>
2019-12-02 12:19:09 +0300

    Result button....you were born on..

--- Js/script.js
+++ Js/script.js
@@ -1,36 +1,39 @@
-function mt(){
-alert("Thank you for your sumission");
-var m = parseInt(document.getElementsByTagName("select")[1].value);
-alert("Your month is " + m);
-var d = parseInt(document.getElementsByTagName("select")[0].value);
-alert("yours date is " + d);
-var y = parseInt(document.getElementsByTagName("input")[0].value);
-alert("your year is " + y);
-var g = document.getElementsByTagName("select")[2].value;
-alert("yours gender is " + g);
-var date = new Date(y,m,d);
-var day = date.getDay();
-alert(day);
-var days = ["Thursday","Friday" , "Sartuday","Sunday" , "Monday" ,"Tuesday", "Wendesday"];
-var days2 = ["Friday","Sartuday","Sunday","Monday","Tuesday","Wednesday" ,"Thursday"];
-var days3 = ["Sartuday" , "Sunday", "Monday" , "Tuesday", "Wednesday","Thursday","Friday"]
-if (m==4){
-    alert(days2[day]);
-}
-else if (m==6){
-    alert(days2[day]);
-}
-else if(m==9){
-    alert(days2[day]);
-}
-else if(m==11){
-    alert(days2[day]);
-}
-else if (m == 2){
-    alert(days3[day]);
-}
-else{
-    alert(days[day]);
-}
+function mt() {
+    alert("Thank you for your sumission");
+    var m = parseInt(document.getElementsByTagName("select")[1].value);
+    var d = parseInt(document.getElementsByTagName("select")[0].value);
+    var y = parseInt(document.getElementsByTagName("input")[0].value);
+    var g = document.getElementsByTagName("select")[2].value;
+    var date = new Date(y, m, d);
+    var day = date.getDay();
+    var days = ["Thursday", "Friday", "Sartuday", "Sunday", "Monday", "Tuesday", "Wendesday"];
+    var days2 = ["Friday", "Sartuday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+    var days3 = ["Sartuday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    var akanmale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var akanfem = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    if (m == 4) {
+        alert( "You were born on " +  days2[day]);
+        document.getElementsById("result2").InnerHTML = "You were born on " + days2[day];
+    }
+    else if (m == 6) {
+        alert( "Yoy were born on " + days2[day]);
+        document.getElementsById("result2").InnerHTML = "You were born on " +  days2[day];
+    }
+    else if (m == 9) {
+        alert( "You were born on " + days2[day]);
+        document.getElementsById("result2").InnerHTML = "Your day is" + days2[day];
+    }
+    else if (m == 11) {
+        alert( "You were born on " + days2[day]);
+        document.getElementsById("result2").InnerHTML = days2[day];
+    }
+    else if (m == 2) {
+        alert( "You were born on " + days3[day]);
+        document.getElementsById("result2").innerHTML = days3[day];
+    }
+    else {
+        alert( "You were born on " + days[day]);
+        document.getElementsById("result2").innerHTML = days[day];
+    }
 
 }
