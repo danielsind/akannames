b65de7f43d2eec4c4d63f9db09170cdb9a45e480
danielsind <danielsindet@gmail.com>
2019-12-02 17:16:41 +0300

    edited comments on js

--- Js/script.js
+++ Js/script.js
@@ -4,20 +4,51 @@ function mt() {
     var d = parseInt(document.getElementsByTagName("select")[0].value);
     var y = parseInt(document.getElementsByTagName("input")[0].value);
     var g = document.getElementsByTagName("select")[2].value;
+
+    // get date for the months given//
+
     var date = new Date(y, m, d);
+
+    //get day of the week//
+
     var day = date.getDay();
+
+    // for months with 31 days//
+
     var days = ["Thursday", "Friday", "Sartuday", "Sunday", "Monday", "Tuesday", "Wendesday"];
+    
+    // for months with 30days//
+
     var days2 = ["Friday", "Sartuday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+
+    // for months with 31//
+
     var days3 = ["Sartuday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+    // akan names for male born on months with 31 days//
     var am1 = ["Yaw","Kofi","Kwame","Kwasi","Kwadwo","Kwabena","Kwaku"];
+
+    // months with 30 days//
     var am2 = ["Kofi","Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw"];
+
+    // months feb //
     var am3 = ["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+
+    // females born on months with 31 days//
     var af1 = ["Yaa","Afua","Ama","Akosua","Adwoa","Abenaa","Akua"];
+
+    // months with 30 days //
     var af2 = ["Afua","Ama","Akosua","Adwoa","Abenaa","Akua","Yaa"];
+
+    //february//
     var af3 = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
+
+    // validity ondition//
     if ( y === "" ){
         alert("Ooops!!! invalid year entry");
     }
+
+    // if male//
     else if ( g == 1){
     if (m == 4 ) {
         alert( "You were born on " +  days2[day] +  " and your Male Akan name is " + am2[day]);
@@ -39,6 +70,8 @@ function mt() {
     }
 
 }
+
+// if female//
 else if ( g == 2){
     if (m == 4 ) {
         alert( "You were born on " +  days2[day] +  " and your Female Akan name is " + af2[day]);
@@ -60,6 +93,7 @@ else if ( g == 2){
     }
 
 }
+// else //
 else {
     alert (" please make sure you have selected something");
 }
